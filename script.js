82891c47b46cb9d853ecfa56e55929fbaeec7e2f
Kshitij Tripathi <pir4cy@MacBook-Pro.local>
2020-08-13 03:23:00 +0530

    to add: option to delete and login

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
-const library = document.querySelector('#library')
+const firebaseConfig = {
+    apiKey: "AIzaSyAjRXAXk7h8E6N--hNQ7W-_LHfKflXSHAA",
+    authDomain: "libray-odin.firebaseapp.com",
+    databaseURL: "https://libray-odin.firebaseio.com",
+    projectId: "libray-odin",
+    storageBucket: "libray-odin.appspot.com",
+    messagingSenderId: "1033906900315",
+    appId: "1:1033906900315:web:e6d8351dc985aedbf2de7e",
+    measurementId: "G-KQDBVJKH5J"
+  };
+  firebase.initializeApp(firebaseConfig);
+
+const library = document.querySelector('#library');
 const form = document.querySelector('#newMovie');
-const formSubmit = document.querySelector('#submit')
-let myLibrary = [];
+const formSubmit = document.querySelector('#submit');
+var database = firebase.database();
+var movieLibrary = database.ref('movieLibrary');
 
 function Movie(title,director,ratings,watched){
     this.title = title;
     this.director = director;
     this.ratings = ratings;
     this.watched = watched; 
-    
-    this.info = function() {
-        return (`${title}<br>${director}<br>${ratings}/5<br>
-        ${watched ? "Watched" : "Not yet"}`);
-    }
 }
+Movie.prototype.info = function() {
+    return (`${this.title}<br>${this.director}<br>${this.ratings}/5<br>
+    ${this.watched ? "Watched" : "Not yet"}`); 
+};
 
 function addMovie() {
     let title = document.forms["newMovie"]["movieTitle"].value;
@@ -21,20 +33,24 @@ function addMovie() {
     let rating = document.forms["newMovie"]["ratingsMovie"].value;
     let watched = document.forms["newMovie"]["movieWatched"].checked;
     const newMovie = new Movie (title,director,rating,watched)
-    myLibrary.push(newMovie)
-
-    const movieDiv = document.createElement('div');
-    movieDiv.setAttribute('id', `movie${myLibrary.length}`);
-    movieDiv.setAttribute('class', 'grid-item');
-    movieDiv.innerHTML = newMovie.info();
-
-    library.appendChild(movieDiv)
+    movieLibrary.push().set(newMovie);
 }
 
+// Rendering movie library
+function renderLibrary(){
+    movieLibrary.on('value', function(snapshot) {
+        library.innerHTML = ''
+        snapshot.forEach(function(userSnapshot){
+            let movie = userSnapshot.val();
+            var movieDiv = document.createElement('div');
+            movieDiv.setAttribute('id', `movie${userSnapshot.val().title}`);
+            movieDiv.setAttribute('class', 'grid-item');
+            var dispMovie = new Movie(movie.title, movie.director, movie.ratings, movie.watched);
+            movieDiv.innerHTML = dispMovie.info();
+            library.appendChild(movieDiv)
+        });
+    });
+};
+
 formSubmit.addEventListener('click',addMovie);
-
-
-// Rmove this function when firebase is implemented
-function handleForm(event) { event.preventDefault(); } 
-form.addEventListener('submit', handleForm);
-console.log(myLibrary)
+renderLibrary();
