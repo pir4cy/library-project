9479e6b0086a0b03ea2d4e1e3480b29800f97abf
Kshitij Tripathi <pir4cy@MacBook-Pro.local>
2020-08-17 14:13:37 +0530

    Add watch button in each card to change watch status

--- script.js
+++ script.js
@@ -13,25 +13,41 @@ const firebaseConfig = {
 const library = document.querySelector('#library');
 const form = document.querySelector('#newMovie');
 const formSubmit = document.querySelector('#submit');
+const buttonWatch = document.querySelector('#watched');
 var database = firebase.database();
 var movieLibrary = database.ref('movieLibrary');
 
-function Movie(title,director,ratings,watched){
-    this.title = title;
-    this.director = director;
-    this.ratings = ratings;
-    this.watched = watched; 
+
+var sliderOutput = document.getElementById("ratingsOutput")
+var slider = document.getElementById("myRange");
+sliderOutput.innerHTML = slider.value;
+
+slider.oninput = function() {
+    sliderOutput.innerHTML = this.value;
+  } 
+class Movie {
+    constructor(title, director, ratings, watched) {
+        this.title = title;
+        this.director = director;
+        this.ratings = ratings;
+        this.watched = watched;
+    }
+    info() {
+        return (`<div class="card-body">
+    <h5 class="card-title">${this.title}</h5>
+    <h6 class="card-subtitle mb-2 text-muted">${this.director}</h6>
+    <a href="#" class="card-link">${this.ratings}/10</a>
+    <p class="card-text">${this.watched ? "Watched" : "Not yet"}</p>
+    </div>`);
+    }
 }
-Movie.prototype.info = function() {
-    return (`${this.title}<br>${this.director}<br>${this.ratings}/5<br>
-    ${this.watched ? "Watched" : "Not yet"}`); 
-};
 
 function addMovie() {
     let title = document.forms["newMovie"]["movieTitle"].value;
     let director = document.forms["newMovie"]["movieAuth"].value;
-    let rating = document.forms["newMovie"]["ratingsMovie"].value;
+    let rating = slider.value;
     let watched = document.forms["newMovie"]["movieWatched"].checked;
+    
     const newMovie = new Movie (title,director,rating,watched)
     movieLibrary.push().set(newMovie);
 }
@@ -44,7 +60,7 @@ function renderLibrary(){
             let movie = userSnapshot.val();
             var movieDiv = document.createElement('div');
             movieDiv.setAttribute('id', `movie${userSnapshot.val().title}`);
-            movieDiv.setAttribute('class', 'grid-item');
+            movieDiv.setAttribute('class', 'grid-item card');
             var dispMovie = new Movie(movie.title, movie.director, movie.ratings, movie.watched);
             movieDiv.innerHTML = dispMovie.info();
             library.appendChild(movieDiv)
